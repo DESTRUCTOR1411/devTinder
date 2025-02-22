af397efa5a04fb44e2cd05f45ad15d81faf75ae6
Raj Rai <rairaj1411@gmail.com>
2025-02-22 11:40:27 +0530

    Exploring get post delete methods

--- src/app.js
+++ src/app.js
@@ -2,10 +2,25 @@ const express= require("express");
 
 const app=express();
 
-app.use("/users",(req,res)=>{
-    res.send("hello from server")
-    console.log("hello from server");
+// app.use("/users",(req,res)=>{
+//     res.send("hello from server")
+//     console.log("hello from server");
+// })
+
+app.get("/user", (req,res)=>{
+    res.send({firstName:"Raj Rai", email:"rairaj1411@gmial.com"});
 })
+
+app.post("/user",(req,res)=>{
+    //saving database to DB
+    res.send("user data is saved successfully!");
+})
+
+app.delete("/user",(req,res)=>{
+    //saving database to DB
+    res.send("user data is deleted successfully!");
+})
+
 
 const port=8000;
 app.listen(port, ()=>{
