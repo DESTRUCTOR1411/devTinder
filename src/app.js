f38ae27d489ce549b12d47c1c57caf4483007c5d
Raj Rai <rairaj1411@gmail.com>
2025-02-23 12:02:02 +0530

    connectin eshtablished with the database

--- src/app.js
+++ src/app.js
@@ -11,15 +11,6 @@ app.get("/user", (req,res)=>{
     res.send({firstName:"Raj Rai", email:"rairaj1411@gmial.com"});
 })
 
-app.post("/user",(req,res)=>{
-    //saving database to DB
-    res.send("user data is saved successfully!");
-})
-
-app.delete("/user",(req,res)=>{
-    //saving database to DB
-    res.send("user data is deleted successfully!");
-})
 
 
 const port=8000;
