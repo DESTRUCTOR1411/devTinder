d480e036c8d29c37060c649cb3a93aa2e914c7e5
Raj Rai <rairaj1411@gmail.com>
2025-02-24 16:31:56 +0530

    completed the patch and diving into api ending

--- src/app.js
+++ src/app.js
@@ -51,6 +51,32 @@ app.get('/feed',async (req,res)=>{
   }
 })
 
+//delete the user by Id
+app.delete("/user", async (req,res)=>{
+  const userId=req.body._id;
+  try{
+    
+    const user= await User.findByIdAndDelete(userId);
+    res.status(200).send("Deleted Successfully!!!")
+
+  }catch(err){
+    res.status(400).send(" connot be Deleted!!!")
+  }
+})
+
+//update data from the database
+app.patch("/user" , async (req,res)=>{
+  const userId=req.body._id;//to find the user id;
+  const data=req.body;///that to be changed
+
+  try{
+    await User.findByIdAndUpdate(userId, data);
+    res.status(200).send("update succesfully!!!!;");
+
+  }catch(err){
+    res.status(400).send("cannot be updated!!!"+err.message);
+  }
+})
 
 
 connectDB()
