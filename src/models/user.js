6c81e919babc9b06a4a010744bb2dd4239411cfb
Raj Rai <rairaj1411@gmail.com>
2025-02-25 18:34:06 +0530

    data validations

--- src/models/user.js
+++ src/models/user.js
@@ -1,18 +1,38 @@
 const mongoose = require('mongoose');
-
+const validator= require('validator')
 //creating the schema first
 const userSchema = new mongoose.Schema({
     firstName:{
         type: String,
+        required:true,
     },
     lastName:{
         type: String,
+        // required:true,
     },
     emailId:{
         type: String,
+        lowercase:true,
+        required:true,
+        unique:true,
+        trim: true,//trim the blank spaces
+
+        validate(value){
+            if(!validator.isEmail(value)){
+                throw new error("user Email is not valid")
+            }
+        }
     },
     password:{
         type: String,
+        required:true,
+        minLength: 8,
+        maxlength: 10,
+        validate(value){
+            if(!validator.isStrongPassword(value)){
+                throw new error("Password is not strong enough")
+            }
+        }
     },
     age:{
         type: Number,
@@ -20,6 +40,15 @@ const userSchema = new mongoose.Schema({
     gender:{
         type: String,
     },
+    photoUrl:{
+        type: String,
+        default: "https://www.bing.com/images/search?view=detai0"
+    },
+    skills:{
+        type: [String],
+    },
+},{
+    timestamp:true,
 })
 
 //creating the model then
